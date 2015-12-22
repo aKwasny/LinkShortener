c5cf7fbaa7d832fb9679401365f536b4426a522e
aKwasny <andrzej.kwasiborski@gmail.com>
2015-12-22 01:49:09 +0100

    Nie wszystko, ale coś. Dalej trzeba działać

--- shortener.py
+++ shortener.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
+import json
+import random
+import string, time, sqlite3
+from math import floor
 from flask import Flask, render_template, request, redirect, session
 from uuid import uuid4
-import time
+from urlparse import urlparse
+from sqlite3 import OperationalError
 
 app_url = '/kwasiboa/session'
 app = Flask(__name__)
 app.secret_key = 'J()@#_R MU$*(_U$#*(T'
+linkbasename = 'links.txt'
+all_links = {}
+all_users = {}
 
 from werkzeug.debug import DebuggedApplication
 
-app.debug = True
+app.debug = False
 app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
 
@@ -32,21 +40,34 @@ def login():
             session['uid'] = uuid4()
             #	uid - unikalny identyfikator
             session['username'] = username
-            return render_template('login_success.html', username=username)
-        return render_template('login_failure.html', username=username)
+            loggedin = True
+            message = u'Zalogowano pomyślnie'
+            return render_template('login_success.html', username=username, loggedin=loggedin, message=message)
+        else:
+            message = u'Błąd logowania.'
+            return render_template('login_form.html', username=username, message=message)
 
 
-('@app.route(app_url + \'/register\', methods=[\'GET\',\'POST\'])\n'
- 'def register():\n'
- '   if request.method == \'GET:\':\n'
- '		return render_template(\'register_form.html\')\n'
- '	if request.method ==\'POST\':\n'
- '		username = request.form.get(\'login\')\n'
- '		password = request.form.get(\'password\')\n'
- '		return render_template(\'register_success.html\', username=username)\n'
- '		print "Sukces! Za 8 sekund zostaniesz przeniesiony do strony logowania"\n'
- '		time.sleep(8)\n'
- '		return redirect(app_rul + \'/login_form.html\')')
+def logout():
+    session.pop('username', None)
+    return redirect("http://edi.iem.pw.edu.pl" + app_url)
+
+
+@app.route(app_url + '/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register_form.html')
+    if request.method == 'POST':
+        username = request.form.get('login')
+        password = request.form.get('password')
+        if username not in all_users:
+            all_users[username] = password
+            message = u'Zarejestrowano pomyślnie'
+        else:
+            message = u'Podana nazwa użytkownika jest już zajęta, wybierz proszę inną'
+        return render_template('register_success.html', username=username, password=password)
+
+
 
 
 @app.route(app_url + '/shortener', methods=['GET', 'POST'])
@@ -58,7 +79,32 @@ def shortener():
     if request.method == 'GET':
         return render_template('shortener.html')
     if request.method == 'POST':
-        link = request.form.get('linktoshorten')
+        linktoshorten = request.form.get('linktoshorten')
+        shortened_link = 'http://edi.iem.pw.edu.pl' + app_url + '/' + ''.join(
+                random.SystemRandom().choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for x in
+                range(7))
+        savelinktobase(shortened_link, 'links.txt')
+        return render_template('link_shortened.html')
+
+
+@app.route(app_url + '/decrypter', methods=['GET', 'POST'])
+def decrypter():
+    if request.method == 'GET':
+        return render_template('decrypter.html')
+    if request.method == 'POST':
+        linktodecrypt = request.form.get('linktodecrypt')
+
+
+def savelinktobase(link, filename):
+    with open(filename, 'w') as saving:
+        saving.write('%s' % json.dumps(all_links))
+        saving.close()
+
+
+def loadlinkfrombase(link, filename):
+    with open(filename, 'r') as loading:
+        all_links.update(json.loads(filename.read()))
+        loading.close()
 
 
 
